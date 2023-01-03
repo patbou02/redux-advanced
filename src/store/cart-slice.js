da1fc9fd9ec42ac3b855f443f72404943d1e1dd4
Patrick Boutin <patrick.boutin@roberthalf.com>
2023-01-02 17:28:42 -0800

    Created removeItemFromCard() method inside the cart store slice.

--- src/store/cart-slice.js
+++ src/store/cart-slice.js
@@ -23,7 +23,18 @@ const cartSlice = createSlice({
         existingItem.totalPrice = existingItem.totalPrice + newItem.price;
       }
     },
-    removeItemFromCart() {}
+    removeItemFromCart(state, action) {
+      const id = action.payload;
+      const existingItem = state.items.find(item => item.id === id);
+      state.totalQuantity--;
+      state.changed = true;
+      if (existingItem.quantity === 1) {
+        state.items = state.items.filter(item => item.id !== id);
+      } else {
+        existingItem.quantity--;
+        existingItem.totalPrice = existingItem.totalPrice - existingItem.price;
+      }
+    }
   }
 });
 
